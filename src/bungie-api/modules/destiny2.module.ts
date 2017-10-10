24d0e33e83b4872878fa9118def5ea282aa4587e
SK83RJOSH <sk83rjosh83@yahoo.com>
2017-10-09 23:23:29 -0400

    Modules: Added additional Destiny APIs

--- src/bungie-api/modules/destiny2.module.ts
+++ src/bungie-api/modules/destiny2.module.ts
@@ -1,20 +1,36 @@
-import {DestinyDefinition, DestinyManifest, DestinyMilestone, DestinyMilestoneDefinition} from '../definitions';
+import * as Defs from '../definitions';
 import {Module} from '../module';
 
 export class Destiny2Module extends Module {
-	public getDestinyManifest(): Promise<DestinyManifest> {
+	public getDestinyManifest(): Promise<Defs.DestinyManifest> {
 		return this.client.get('Destiny2/Manifest');
 	}
 
-	public getDestinyEntityDefinition<T = DestinyDefinition>(entityType: string, itemHash: number): Promise<T> {
+	public getDestinyEntityDefinition<T = Defs.DestinyDefinition>(entityType: string, itemHash: number): Promise<T> {
 		return this.client.get(`Destiny2/Manifest/${entityType}/${itemHash}`);
 	}
 
-	public getDestinyMilestoneDefinition(milestoneHash: number): Promise<DestinyMilestoneDefinition> {
-		return this.getDestinyEntityDefinition<DestinyMilestoneDefinition>('DestinyMilestoneDefinition', milestoneHash);
+	public getDestinyMilestoneDefinition(milestoneHash: number): Promise<Defs.DestinyMilestoneDefinition> {
+		return this.getDestinyEntityDefinition<Defs.DestinyMilestoneDefinition>('DestinyMilestoneDefinition', milestoneHash);
 	}
 
-	public getClanWeeklyRewardState(groupId: number): Promise<DestinyMilestone> {
+	public searchDestinyPlayer(displayName: string, membershipType: Defs.BungieMembershipType = Defs.BungieMembershipType.All): Promise<Defs.UserInfoCard[]> {
+		return this.client.get(`Destiny2/SearchDestinyPlayer/${Defs.BungieMembershipType[membershipType]}/${displayName}`);
+	}
+
+	public getProfile(membershipType: Defs.BungieMembershipType, membershipId: string, components: Defs.DestinyComponentType[] = [Defs.DestinyComponentType.Profiles]): Promise<Defs.DestinyProfileResponse> {
+		return this.client.get(`Destiny2/${Defs.BungieMembershipType[membershipType]}/Profile/${membershipId}`, {
+			components: components.join(','),
+		});
+	}
+
+	public getCharacter(membershipType: Defs.BungieMembershipType, membershipId: string, characterId: string, components: Defs.DestinyComponentType[] = [Defs.DestinyComponentType.Characters]): Promise<Defs.DestinyCharacterResponse> {
+		return this.client.get(`Destiny2/${Defs.BungieMembershipType[membershipType]}/Profile/${membershipId}/Character/${characterId}`, {
+			components: components.join(','),
+		});
+	}
+
+	public getClanWeeklyRewardState(groupId: number): Promise<Defs.DestinyMilestone> {
 		return this.client.get(`Destiny2/Clan/${groupId}/WeeklyRewardState`);
 	}
 }
